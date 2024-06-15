73cc3230324cdaa99975f7f352a759400a071c22
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-06-15 14:39:24 -0600

    updated css for nav bar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import './Contact.css'; // import your CSS file
+
 
 const Contact = () => {
   const [formData, setFormData] = useState({
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,31 +5,51 @@ import './Header.css';
 const Header = () => {
   return (
     <header>
-      <nav className="navbar">
-        <div className="navbar-brand">
-        <img src="https://i.postimg.cc/xCbt1DW3/Screenshot-2024-06-15-at-11-01-12-AM-removebg-preview.png" alt="Zay T Shirt Design Logo" className="logo"/>        </div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/services" className="nav-link">
-              Services
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">
-              Contact
-            </Link>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            <img
+              src="https://i.postimg.cc/xCbt1DW3/Screenshot-2024-06-15-at-11-01-12-AM-removebg-preview.png"
+              alt="Zay T Shirt Design Logo"
+              className="logo"
+            />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/services" className="nav-link">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
